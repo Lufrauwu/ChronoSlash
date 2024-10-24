b64a1a9a98d08d7025723f6779f9033f3a814e09
Sebastiantorres01 <127338259+Sebastiantorres01@users.noreply.github.com>
2024-10-24 15:08:12 -0600

    Cambios de Jueves

---
+++ Assets/[Scripts]/Art/Emissive_Flicker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emissive_Flicker : MonoBehaviour
+{
+	public AnimationCurve EmissiveCurve;
+	public float CurveSpeed = 1f;
+
+
+	private void Awake()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+}
