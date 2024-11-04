a45bac2f1b34719be170d2585feb1220d40cd34f
Luis Francisco Pérez Hernández <lexius002@gmail.com>
2024-11-04 14:23:50 -0600

    AttackTriggerer

--- Assets/[Scripts]/PlayerScripts/CombatSystem/AttackTriggerer.cs
+++ Assets/[Scripts]/PlayerScripts/CombatSystem/AttackTriggerer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AttackTriggerer : MonoBehaviour
 {
     [SerializeField] private int[] savedCombo;
+    [SerializeField] private Animator animator;
     void Start()
     {
         
@@ -22,6 +23,7 @@ public class AttackTriggerer : MonoBehaviour
         {
             case "000000":
                Debug.Log("hOLA");
+               animator.SetBool("Slash", true);
                 break;
             case "001100":
                 Debug.Log("SON 5");
@@ -29,6 +31,41 @@ public class AttackTriggerer : MonoBehaviour
             case "000111":
                 Debug.Log("SON 4");
                 break;
+            case "111000":
+                break;
+            case "110011":
+                break;
+            case "100010":
+                break;
+            case "111111":
+                break;
+            case "00000":
+                break;
+            case "00011":
+                break;
+            case "11000":
+                break;
+            case "11100":
+                break;
+            case "11111":
+                break;
+            case "0000":
+                break;
+            case "0001":
+                break;
+            case "0011":
+                break;
+            case "0101":
+                break; 
+            case "1000":
+                break;  
+            case "1100":
+                Debug.Log("SI");
+                break;
+            case "1110":
+                break;
+            
+
             
         }
     }
