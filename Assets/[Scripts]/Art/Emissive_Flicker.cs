f923bdce6dfc521b30b6806a883d076de75aa0bb
Sebastiantorres01 <127338259+Sebastiantorres01@users.noreply.github.com>
2024-11-05 15:18:49 -0600

    Sebas_PC

--- Assets/[Scripts]/Art/Emissive_Flicker.cs
+++ Assets/[Scripts]/Art/Emissive_Flicker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,27 @@ public class Emissive_Flicker : MonoBehaviour
 	public AnimationCurve EmissiveCurve;
 	public float CurveSpeed = 1f;
 
+	public Material Emmaterial;
+    //private MeshRenderer Emission;
+    private float Intensity;
 
-	private void Awake()
-	{
-		
-	}
 
+
+
+    private void Awake()
+    {
+        //Emission = GetComponent<MeshRenderer>();
+        Intensity = Emmaterial.GetFloat("EmissionIntensity");
+
+    }
 	void Update()
 	{
-		
-	}
+        //Obtener Emisivo de un material
+        //No esta detectando el Material de los Objetos
+        //Emission.GetComponent<Material>().SetFloat("Emission Intensity", (Intensity * EmissiveCurve.Evaluate(Time.time * CurveSpeed)));
+        //No esta Moviendo el valor que se le pide
+        //Intensity = Intensity*EmissiveCurve.Evaluate(Time.time*CurveSpeed);
+        print(Intensity);
+        //Emmaterial.SetFloat(Intensity, (Intensity * EmissiveCurve.Evaluate(Time.time * CurveSpeed)));
+    }
 }
