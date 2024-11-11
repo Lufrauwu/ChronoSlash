c95ca284f7d0ed7ac0e013c7005dfaf4f6c0a9a3
Sebastiantorres01 <127338259+Sebastiantorres01@users.noreply.github.com>
2024-11-11 15:12:08 -0600

    Sebas_FinDeSemana

--- Assets/[Scripts]/Art/Elevador.cs
+++ Assets/[Scripts]/Art/Elevador.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Elevador : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
+    private int LocationsIndex = 0;
+    public GameObject[] locations;
+    private void OnTriggerEnter(Collider other)
     {
-      
+     if(other.gameObject.tag == "Player")
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, locations[1].position, 2 * Time.deltaTime);
+            print("PlayerOn");
+        }
+        else
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, locations[0].position, 2 * Time.deltaTime);
+            print("PlayerOff");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
