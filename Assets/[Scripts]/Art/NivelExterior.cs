cba7e77e630be108d277622cda8a4e465366f686
Sebastiantorres01 <storresbuilds@gmail.com>
2024-11-11 19:27:08 -0600

    Sebas_Lunes

---
+++ Assets/[Scripts]/Art/NivelExterior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NivelExterior : MonoBehaviour
+{
+    //Arrays de objetos que el jugador va a coleccionar
+    public GameObject[] Objetos;
+    public GameObject[] ObjetosJugador;
+
+    private bool PickedUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Colission other)
+    {
+
+    }
+}
