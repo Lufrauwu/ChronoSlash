67ffcf0fb216a7b933f61046f00151bfb6620254
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2024-12-03 00:31:03 -0700

    healthBar

--- Assets/[Scripts]/PlayerScripts/PlayerLocomotion/HealthController.cs
+++ Assets/[Scripts]/PlayerScripts/PlayerLocomotion/HealthController.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class HealthController : MonoBehaviour
 {
     [SerializeField] private int currentHealth = 100;
     [SerializeField] private int maxHealth = 100;
+    public Action<float, bool> OnHealthChanged;
     void Start()
     {
         currentHealth = maxHealth;
@@ -14,5 +14,22 @@ public class HealthController : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        OnHealthChanged.Invoke(currentHealth, true);
+    }
+
+    public void Heal(int healthToHeal)
+    {
+        currentHealth += healthToHeal;
+        OnHealthChanged.Invoke(currentHealth, false);
+    }
+    
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
     }
 }
